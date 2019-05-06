e305e19f6ee8f9237e7c77638e395d86332d4a78
Danielle Martinoli <daniechris5@gmail.com>
2019-05-06 16:57:54 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,8 +52,8 @@ def runner
   card_total = initial_round
   while card_total < 21
   hit?(card_total)
-  card_total == 21
+  card_total += deal_card
   display_card_total(card_total)
   end
-  end_game
+  end_game(card_total)
 end
